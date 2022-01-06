3ded860e7ff63194b108913ab62238dbf5652235
Ivo Calado <ivo.calado@ifal.edu.br>
2022-01-06 10:49:28 -0300

    Update
    Signed-off-by: Ivo Calado <ivo.calado@ifal.edu.br>

--- terraform-tests/main.tf
+++ terraform-tests/main.tf
@@ -12,8 +12,17 @@ terraform {
       source  = "kreuzwerker/docker"
       version = "~> 2.13.0"
     }
+  random = {
+      source = "hashicorp/random"
+  }
+
   }
 }
+
+resource "random_id" "rand" {
+  byte_length = 2
+}
+
 
 resource "docker_image" "nginx" {
   name         = "nginx:latest"
@@ -22,7 +31,7 @@ resource "docker_image" "nginx" {
 
 resource "docker_container" "nginx" {
   image = docker_image.nginx.latest
-  name  = "tutorial"
+  name  = "tutorial-${random_id.rand.hex}"
   ports {
     internal = 80
     external = 8000
